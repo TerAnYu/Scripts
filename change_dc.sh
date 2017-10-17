2cc91a84c1c40e1331b79977135a9e50481fa3d3
Teremshonok Anton <teranyu@yandex.com>
2017-10-17 18:03:09 +0700

    Create change_dc.sh

---
+++ change_dc.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# Задаём порты для локального назначения с удалённого адреса
+#PORTS="389 636 3268 3269 88 9389 464"
+PORTSS="389,636,3268,3269,88,9389,464"
+
+# Destinations
+contr1="10.150.100.10"
+contr2="10.150.100.11"
+contr3="10.150.102.10"
+contr4="10.150.102.11"
+eths=""
+
+ping_cmd="ping -q -c 1 "
+
+
+if ${ping_cmd} $contr1 ; then
+    eths=$contr1
+    echo success dc1
+elif ${ping_cmd} $contr2; then
+    eths=$contr2
+    echo success dc2
+elif ${ping_cmd} $contr3; then
+    eths=$contr3
+    echo success dc3
+elif ${ping_cmd} $contr4; then
+    eths=$contr4
+    echo success dc4
+else
+    echo failure
+fi
+
+
+# Создаём файл при переключении и если этот файл соответствует установленному адресу, то ничего не делаем, иначе переключаем на новый адрес и блокируем его
+if [ -f "/tmp/${eths}.lpta" ]; then
+#    echo "Адрес ${eths} уже указан"
+    echo
+else
+    rm -f /tmp/*.lpta
+    echo "Указываем адрес ${eths}"
+    iptables -t nat -v -L OUTPUT -n --line-number | grep -w localpottoaddress | grep -w tcp | awk '{system ("iptables -t nat -D OUTPUT " $1)}'
+    iptables -t nat -A OUTPUT -m addrtype --src-type LOCAL --dst-type LOCAL -p tcp -m multiport --dport ${PORTSS} -j DNAT --to-destination ${eths} -m comment --comment "localpottoaddress"
+sleep 2
+    iptables -t nat -v -L OUTPUT -n --line-number | grep -w localpottoaddress | grep -w udp | awk '{system ("iptables -t nat -D OUTPUT " $1)}'
+    iptables -t nat -A OUTPUT -m addrtype --src-type LOCAL --dst-type LOCAL -p udp -m multiport --dport ${PORTSS} -j DNAT --to-destination ${eths} -m comment --comment "localpottoaddress"
+
+    touch /tmp/${eths}.lpta
+fi
+
+# Проверяем на наличие маскарада, если есть, то ничего не делаем, если нет, удаляем если есть и прописываем снова
+if [ -n "`iptables -t nat -v -L POSTROUTING -n --line-number | grep -w localporttoaddress`" ]
+then
+    echo
+else
+# https://serverfault.com/questions/247623/iptables-redirect-local-connections-to-remote-system-port
+    sysctl -w net.ipv4.conf.all.route_localnet=1
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+    iptables -t nat -v -L POSTROUTING -n --line-number | grep -w localporttoaddress | awk '{system ("iptables -t nat -D POSTROUTING " $1)}'
+    iptables -t nat -A POSTROUTING -m addrtype --src-type LOCAL --dst-type UNICAST -j MASQUERADE -m comment --comment "localporttoaddress"
+fi
+
+exit 0
