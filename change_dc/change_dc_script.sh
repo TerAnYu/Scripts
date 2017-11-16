8afe55f9c90efceaab20be5a81952f4b6b4fb120
Teremshonok Anton <teranyu@yandex.com>
2017-11-16 17:00:48 +0700

    Update change_dc_script.sh
    Вынесение в переменные путей до бинарников

--- change_dc/change_dc_script.sh
+++ change_dc/change_dc_script.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# https://unix.stackexchange.com/questions/217284/not-all-shell-scripts-working-with-crontab
+set PATH=/usr/sbin:/sbin:/usr/bin:/bin
 . $(dirname $0)/change_dc_conf.sh
 
 # https://devidiom.blog/2015/12/03/simple-bash-server-check-script/
@@ -22,32 +25,33 @@ fi
 
 # Создаём файл при переключении и если этот файл соответствует установленному адресу, то ничего не делаем, иначе переключаем на новый адрес и блокируем его
 if [ -f "/tmp/${eths}.lpta" ]; then
-#    echo "Адрес ${eths} уже указан"
-    echo
+    echo "Адрес ${eths} уже указан"
+    echo ----------
 else
     rm -f /tmp/*.lpta
     echo "Указываем адрес ${eths}"
-    iptables -t nat -v -L OUTPUT -n --line-number | grep -w localpottoaddress | grep -w tcp | awk '{system ("iptables -t nat -D OUTPUT " $1)}'
-    iptables -t nat -A OUTPUT -m addrtype --src-type LOCAL --dst-type LOCAL -p tcp -m multiport --dport ${PORTSS} -j DNAT --to-destination ${eths} -m comment --comment "localpottoaddress"
+    ${biniptables} -t nat -v -L OUTPUT -n --line-number | grep -w ${comment} | grep -w tcp | awk '{system ("${biniptables} -t nat -D OUTPUT " $1)}'
+    ${biniptables} -t nat -A OUTPUT -m addrtype --src-type LOCAL --dst-type LOCAL -p tcp -m multiport --dport ${PORTSS} -j DNAT --to-destination ${eths} -m comment --comment "${comment}"
 sleep 2
-    iptables -t nat -v -L OUTPUT -n --line-number | grep -w localpottoaddress | grep -w udp | awk '{system ("iptables -t nat -D OUTPUT " $1)}'
-    iptables -t nat -A OUTPUT -m addrtype --src-type LOCAL --dst-type LOCAL -p udp -m multiport --dport ${PORTSS} -j DNAT --to-destination ${eths} -m comment --comment "localpottoaddress"
+    ${biniptables} -t nat -v -L OUTPUT -n --line-number | grep -w ${comment} | grep -w udp | awk '{system ("${biniptables} -t nat -D OUTPUT " $1)}'
+    ${biniptables} -t nat -A OUTPUT -m addrtype --src-type LOCAL --dst-type LOCAL -p udp -m multiport --dport ${PORTSS} -j DNAT --to-destination ${eths} -m comment --comment "${comment}"
 
     touch /tmp/${eths}.lpta
 fi
 
 # Проверяем на наличие маскарада, если есть, то ничего не делаем, если нет, удаляем если есть и прописываем снова
-if [ -n "`iptables -t nat -v -L POSTROUTING -n --line-number | grep -w localporttoaddress`" ]
+if [ -n "`${biniptables} -t nat -v -L POSTROUTING -n --line-number | grep -w ${comment}`" ]
 then
     echo
 else
 # https://serverfault.com/questions/247623/iptables-redirect-local-connections-to-remote-system-port
 # (which works only in kernels >= 3.6)
 # https://superuser.com/questions/661772/iptables-redirect-to-localhost
-    sysctl -w net.ipv4.conf.all.route_localnet=1
-    sysctl -w net.ipv4.ip_forward=1
-    iptables -t nat -v -L POSTROUTING -n --line-number | grep -w localporttoaddress | awk '{system ("iptables -t nat -D POSTROUTING " $1)}'
-    iptables -t nat -A POSTROUTING -m addrtype --src-type LOCAL --dst-type UNICAST -j MASQUERADE -m comment --comment "localporttoaddress"
+    ${binsysctl} -w net.ipv4.conf.all.route_localnet=1
+    ${binsysctl} -w net.ipv4.ip_forward=1
+    ${biniptables} -t nat -v -L POSTROUTING -n --line-number | grep -w ${comment} | awk '{system ("${biniptables} -t nat -D POSTROUTING " $1)}'
+    ${biniptables} -t nat -A POSTROUTING -m addrtype --src-type LOCAL --dst-type UNICAST -j MASQUERADE -m comment --comment "${comment}"
+    echo ----------
 fi
 
 exit 0
